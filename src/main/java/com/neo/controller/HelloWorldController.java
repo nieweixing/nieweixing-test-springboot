c71070e47efc4c59e9e656aaaacf64468f6e12b1
niewx <nwx_qdlg@163.com>
2021-04-03 09:08:13 +0800

    更新代码

--- src/main/java/com/neo/controller/HelloWorldController.java
+++ src/main/java/com/neo/controller/HelloWorldController.java
@@ -5,8 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloWorldController {
+
+
 
     protected Logger logger =  LoggerFactory.getLogger(this.getClass());
 	
