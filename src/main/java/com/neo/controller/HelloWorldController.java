0e259a20f7f7359bef2f7ee328140f7021193d32
niewx <nwx_qdlg@163.com>
2021-04-03 13:47:52 +0800

    commit

--- src/main/java/com/neo/controller/HelloWorldController.java
+++ src/main/java/com/neo/controller/HelloWorldController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
 
 
 @RestController
